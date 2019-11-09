c3c4b56009bae81cd41e5c34fa076d27fafaa6fe
Anewil <bartekwwb@gmail.com>
2019-11-09 22:46:21 +0100

    Changed allowed hosts

--- game_stats_analysis/settings.py
+++ game_stats_analysis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd%0)cqr2)!op_zi@)@$(b-dxe4y$yda(vmu7*&wb5@$@zaie9f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
