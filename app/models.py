c1ecc00cfb1ddb0d6f863d00c779e9ab914d20c2
Anewil <bartekwwb@gmail.com>
2019-11-16 16:46:44 +0100

    Updated models

--- app/models.py
+++ app/models.py
@@ -11,6 +11,9 @@ class Game(models.Model):
     peak_date = models.DateField(default=date.today)
     max_users = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.name
+
 
 class Player(models.Model):
     id = models.CharField(primary_key=True, max_length=17)
@@ -22,11 +25,20 @@ class Player(models.Model):
         PlayersUpdater.update_nicknames_and_avatars([self.id])
         super(Player, self).save(*args, **kwargs)
 
+    def __str__(self):
+        return self.nickname
+
 
 class GlobalStats(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     users = models.IntegerField()
     date = models.DateTimeField(default=datetime.today)
+
+    def __str__(self):
+        return self.game.__str__() + ' ' + self.date.strftime("%Y-%m-%d %H:%M:%S")
+
+    class Meta:
+        verbose_name_plural = 'Global stats'
 
 
 class Achievement(models.Model):
@@ -34,12 +46,18 @@ class Achievement(models.Model):
     achievement = models.CharField(max_length=200)
     global_percent = models.FloatField()
 
+    def __str__(self):
+        return self.game.__str__() + ' ' + self.achievement
+
 
 class PlayerAchievement(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
     achieved = models.BooleanField()
     unlock_time = models.DurationField()
+
+    def __str__(self):
+        return self.player.__str__() + ' ' + self.achievement.__str__()
 
 
 class PlayerStats(models.Model):
@@ -49,3 +67,8 @@ class PlayerStats(models.Model):
     date = models.DateField(default=date.today)
     stats = JSONField()
 
+    def __str__(self):
+        return self.game.__str__() + ' ' + self.player.__str__()
+
+    class Meta:
+        verbose_name_plural = 'Player stats'
