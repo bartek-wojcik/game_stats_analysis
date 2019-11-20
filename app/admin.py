8c11dd3f300b79d849c92809a2947ee1e9084700
Anewil <bartekwwb@gmail.com>
2019-11-20 15:43:30 +0100

    Changed admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.apps import apps
 
+from app.models import GlobalStats, PlayerStats, Player, Game, Achievement, PlayerAchievement
+
 app = apps.get_app_config('app')
 
-for model_name, model in app.models.items():
-    admin.site.register(model)
+
+class GameFilter(admin.ModelAdmin):
+    list_filter = ('game__name',)
+
+
+admin.site.register(GlobalStats, GameFilter)
+admin.site.register(PlayerStats, GameFilter)
+admin.site.register(Player)
+admin.site.register(Game)
+admin.site.register(Achievement)
+admin.site.register(PlayerAchievement)
+
+admin.site.site_header = 'Game Stats Analysis'
