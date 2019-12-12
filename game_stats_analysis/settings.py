3d44e49d489d9aa642112cd31ad4cfd0547be9cd
Anewil <bartekwwb@gmail.com>
2019-12-12 16:18:08 +0100

    Removed pagination

--- game_stats_analysis/settings.py
+++ game_stats_analysis/settings.py
@@ -44,9 +44,7 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 1000
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
 
 MIDDLEWARE = [
